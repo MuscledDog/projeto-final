6befc1bc010eef67a020179b6c4524a64a66d8fa
MuscledDog <lemos.vasco@gmail.com>
2026-01-22 21:47:30 +0000

    correções de header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
-import { Homes } from './pages/homes/homes';
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Sobre } from './pages/sobre/sobre';
+import { Welcome } from './pages/welcome/welcome';
 
 export const routes: Routes = [
   /* Nossa 1ª rota */
     {path:'', redirectTo:"welcome", pathMatch:'full'},
 
-    {path:"welcome", loadComponent: () => import('./pages/welcome/welcome') .then ((vasco) => vasco.Welcome)} ,
+    {path:"welcome", loadComponent: () => import('./pages/welcome/welcome') .then ((a) => a.Welcome)} ,
 
-    {path:"pages/login", loadComponent: () => import("./pages/login/login") .then (ze => ze.Login)},
+    {path:"pages/login", loadComponent: () => import("./pages/login/login") .then (b => b.Login)},
+    {path:'**', loadComponent:() => import('./pages/sobre/sobre') .then(c => c.Sobre)},
 
   /** Rota padrão sem lazyload */
 
-    {path:"pages/homes", component: Homes},
+    {path:"pages/welcome", component: Welcome},
 
 
     /** Nossa ultima rota */
